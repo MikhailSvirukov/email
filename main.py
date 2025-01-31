54d5c946acde1a595a9adce360d655c42a445587
Mikhail <mihailsvirukov@mail.ru>
2025-01-31 19:55:44 +0300

    add filters of date, sender and email sender has decoding checks date input working with only format

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import imaplib
 import email
 import sys
+from email.header import decode_header
+from datetime import datetime
 
 def connect():
     mail_pass = "mry7SdQtf1fPXiMqekbi"
@@ -9,9 +11,6 @@ def connect():
     imap = imaplib.IMAP4_SSL(imap_server)
     imap.login(username, mail_pass)
     return imap
-
-
-
 
 def find_message_id(value, folder, imap):
     check = 0
@@ -25,8 +24,54 @@ def find_message_id(value, folder, imap):
     if not check:
         print("No such email")
 
+def find_message_email(value, folder, imap):
+    check = 0
+    for item in folder:
+        res, msg = imap.uid("fetch", item, "(RFC822)")
+        msg = email.message_from_bytes(msg[0][1])
+        if msg["From"].split(" ")[1].rstrip(">").lstrip("<") == value:
+            print("Bingo")
+            check = 1
+            break
+    if not check:
+        print("No such email")
 
+def find_message_sender(value, folder, imap):
+    check = 0
+    for item in folder:
+        res, msg = imap.uid("fetch", item, "(RFC822)")
+        msg = email.message_from_bytes(msg[0][1])
+        encoding=decode_header(msg["From"])[0][1]
 
+        if not encoding is None:
+            name = decode_header(msg["From"])[0][0].decode(encoding)
+        else:
+            name = decode_header(msg["From"])[0][0].split(" ")[0]
+        if name == value:
+            print("Bingo")
+            check = 1
+            break
+    if not check:
+        print("No such email")
+
+def find_message_date(value, folder, imap, mark):
+    for item in folder:
+        res, msg = imap.uid("fetch", item, "(RFC822)")
+        msg = email.message_from_bytes(msg[0][1])
+        date=msg["Date"].split(" ")
+        date=date[2]+" "+date[1]+", " +date[3]
+        date_email = datetime.strptime(date, "%b %d, %Y").date()
+        date_value = datetime.strptime(value, "%Y-%m-%d").date()
+        if mark=="from":
+            if date_email >= date_value:
+                print("YES")
+            else:
+                print("NO")
+        else:
+            if date_email <= date_value:
+                print("YES")
+            else:
+                print("NO")
 
 
 def main():
@@ -40,6 +85,16 @@ def main():
     for arg in argv:
         if arg[:5]=="--id=":
             find_message_id(arg[5:], folder, imap)
+        elif arg[:8]=="--email=":
+            find_message_email(arg[8:], folder, imap)
+        elif arg[:9]=="--sender=":
+            find_message_sender(arg[9:], folder, imap)
+        elif arg[:12]=="--date-from=":
+            find_message_date(arg[12:], folder, imap, "from")
+        elif arg[:10]=="--date-to=":
+            find_message_date(arg[10:], folder, imap, "to")
+
+    imap.logout()
 
 
 
